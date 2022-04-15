4ff89a462c431bc978fc859beb441e14dba6f8c7
DiegoReyesCipo <diegoreyescipo@gmail.com>
2022-04-15 15:35:42 -0300

    proyectofuncionando

--- MarDelDiego.js
+++ MarDelDiego.js
@@ -4,23 +4,71 @@ let productos = [
     {id: 2, titulo: 'Napolitano Ganador', precio: 12000, stock: 10, superficie: 250},
     {id: 3, titulo: 'Campeon Mexico 86', precio: 15000, stock: 10, superficie: 300},
 ]
-
+//Variables
 let precio = 10000;
 let valor = 10000;
 let interes = 1.3;
 let precioFinanciado = 0;
-
+let datos= 0;
+let  seleccionAve= 0 ;
+let  seleccionCalle= 0 ;
+// Api Dolar
 const URL= 'https://criptoya.com/api/dolar'
+
+////DECLARE ARRAY  Avenidas
+const avenidas = ['Diego Armando Maradona','Argentina', 'Mexico 86', 'Dalma', 'Gianina','Segurola y Habana','Mano de Dios','La pelota no se mancha'];
+//DECLARE VARIABLE ,  FOREACH list options
+let innerSelectAve = '';
+avenidas.forEach((avenida, index) => innerSelectAve += `<option value='${index}'>${avenida}</option>`)
+//add SELECT Y listen EVENT CHANGE
+$('body #theForm').prepend(`<select id="selectAvenida">${innerSelectAve}</select>`);
+
+//DECLARE ARRAY DE Calles
+const calles = ['Me cortaron las piernas', 'Se te escapo la tortuga', 'Lastima a nadie', 'Claudia','Don Diego','Futbol'];
+//DECLARE VARIABLE ,  FOREACH list options
+let innerSelectCalle = '';
+calles.forEach((calle, index) => innerSelectCalle += `<option value='${index}'>${calle}</option>`)
+//add SELECT Y listen EVENT CHANGE
+$('body #theForm').prepend(`<select id="selectCalle">${innerSelectCalle}</select>`);
+
+
+
+
+
+const elegAve = document.getElementById('selectAvenida')
+elegAve.onchange = (e) => {
+seleccionAve = avenidas[e.target.value]
+avenidaElegida.innerText = 'Has seleccionado como nombre de Avenida : '+ (seleccionAve);
+
+ console.log(typeof(seleccionAve))// Cambiar al DOM en lugar de console log
+ localStorage.setItem('SeleccionDeAvenida',seleccionAve)
+}
+
+const elegCal = document.getElementById('selectCalle')
+elegCal.onchange = (e) => {
+seleccionCalle = calles[e.target.value]
+calleElegida.innerText = 'Has seleccionado como nombre de Calle : '+ (seleccionCalle);
+
+ console.log(typeof(seleccionCalle))// Cambiar al DOM en lugar de console log
+ localStorage.setItem('SeleccionDeCalle',seleccionCalle)
+}
+
+
+//Button show form
+$('#search').on('click', function(){
+  $('#theForm').show();
+})
 
 const dropDown = document.getElementById('opcionTerreno')
 const btn = document.getElementById('btn')
 let valorInput= 0;
 
-// llamar dato del select y traer precio
+// capture dropbox value
 dropDown.onchange = (e) => {
     valorInput = e.target.value
-    console.log(e.target.value)// Trae el texto dependiendo el del select
+    console.log(e.target.value)
 }
+//btn onclick function compare value select with array
 btn.onclick = (e)=> {
     const existe = productos.find(x => x.titulo === valorInput);// comparo los dos textos 
     existe ?  valor = existe.precio : console.log('NO hay Nada');
@@ -33,14 +81,14 @@ btn.onclick = (e)=> {
  
   );
 }
-console.log("valor fuera onclick",valor);/// En su momento probe con precio como variable aca y en la fx. pero no funciono
 
-//guardar productos en local Storage
+
+//Store List of products in local Storage 
 localStorage.setItem( "Listaproductos", JSON.stringify(productos));
 productos=JSON.parse(localStorage.getItem("Listaproductos"));
 console.log(typeof(productos));
 
-  //prueba aplicar after input para cuotas      
+  //function get parcels and price     
   function addElementToList (){
     const inputRef = document.getElementById('inputPago');
     const value = inputRef.value;
@@ -51,15 +99,16 @@ console.log(typeof(productos));
       else if (value == 1) {
             
                 
-               resultado.innerText = 'Total a pagar por el lote'+ valor//cambie alert por DOM innerText resultado
+               resultado.innerText = 'Total a pagar por el lote'+ valor
                console.log(value);
       }
       else if (value == 6 || value == 10){
           
         precioFinanciado = valor * interes// precio la tengo que actualizar a valor
         
-      resultado.innerText = 'el precio financiado es: $' +(  precioFinanciado);//calcular cuotas
-// falta calcular valor cuotas y mostrarlo en DOM
+      resultado.innerText = 'el precio financiado es: $' +(  precioFinanciado);
+     
+
         
      //funcion flecha no Me funcionaba
         
@@ -74,31 +123,11 @@ console.log(typeof(productos));
      .then(respuesta => respuesta.json())
      .then (datos => {
          
-         console.log("Console log datos.blue", + datos.blue);
+      
+      dolar.innerText = 'Cotizacion del dolar hoy: $' +(datos.blue);
          let totalBlue = datos.blue;
-     
-           // agregarNameAlDOM()// recien aca llamo la funcion para modificar DOM
-     
+         console.log("total pesos $", valor * datos.blue );
+           
      });
-     //console.log(typeof(datos));
-     console.log("total", precio * datos.blue );
-     const fetchBlue = () => {
-         fetch (URL)
-         .then(res=> res.json())
-         .then( datos => {
-             let totalBlue = datos.blue;
-             console.log('console log fetch.Blue', totalBlue );
-         }) 
-     };
+
     
-     
-
-     window.onload=function(){
-      document.getElementById('contenido').style.display="none";
-      }
-   function contenido()
-   {
-          
-       document.getElementById("contenido").style.display="block";
-   }
-
